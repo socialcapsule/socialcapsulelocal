39f16858787b2af5d938948ceec4bf284813275a
socialcapsule <socialcapsule899@gmail.com>
2017-01-19 12:20:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://socialcapsulelocal:betabulls@123@ds117819.mlab.com:17819/socialcapsilelocal',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'kqBeRpqbXvxyVCoJla92SV5be9QjQJlk8wu6YmBm',
-  masterKey: process.env.MASTER_KEY || 'SV5be9QjQJlk8wu6YmBmlgcYdZxzKEcS91M0OJRo', //Add your master key here. Keep it secret!
-  clientKey:process.env.CLIENT_KEY ||  'lgcYdZxzKEcS91M0OJRokqBeRpqbXvxyVCoJla92',//Add your client key here. Keep it secret!
+  appId: process.env.APP_ID || 'kqBeRpqbXvxyVCoJla92',
+  masterKey: process.env.MASTER_KEY || 'SV5be9QjQJlk8wu6YmBm', //Add your master key here. Keep it secret!
+  clientKey:process.env.CLIENT_KEY ||  'lgcYdZxzKEcS91M0OJRo',//Add your client key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
